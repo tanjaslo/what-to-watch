d39f7ac963b05bcf528d49dbccd17f318c57f9f2
Tanja <tanja.slo@ukr.net>
2021-10-14 22:29:42 +0200

    Adds components for all pages.

--- project/src/components/pages/my-list-page/my-list-page.tsx
+++ project/src/components/pages/my-list-page/my-list-page.tsx
@@ -1,4 +1,4 @@
-function MyList(): JSX.Element {
+function MyListPage(): JSX.Element {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -130,4 +130,4 @@ function MyList(): JSX.Element {
   );
 }
 
-export default MyList;
+export default MyListPage;
