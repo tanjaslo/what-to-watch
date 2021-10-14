4471168f9573b56e3238ed9ae733d12005379763
Tanja <tanja.slo@ukr.net>
2021-10-14 12:20:44 +0200

    Adds user-page and my-list components.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import MainPage from '../main-page/main-page';
+import MainPage from '../pages/main-page/main-page';
 
 type AppScreenProps = {
   filmsCount: number;
--- project/src/components/pages/main-page/main-page.tsx
+++ project/src/components/pages/main-page/main-page.tsx
@@ -1,4 +1,4 @@
-import FilmCard from '../film-card/film-card';
+import FilmCard from '../../film-card/film-card';
 
 type MainPageProps = {
   filmsCount: number;
