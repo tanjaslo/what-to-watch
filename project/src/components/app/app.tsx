cb8b9b3c35a0baa18156226e594078fefc90b134
Tanja <tanja.slo@ukr.net>
2021-10-16 00:20:38 +0200

    Adds routes.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,9 @@
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import AddReviewPage from '../pages/add-review-page/add-review-page';
 import MainPage from '../pages/main-page/main-page';
+import MoviePage from '../pages/movie-page/movie-page';
+import MyListPage from '../pages/my-list-page/my-list-page';
+import PlayerPage from '../pages/player-page/player-page';
 import UserPage from '../pages/user-page/user-page';
 
 type AppScreenProps = {
@@ -23,6 +27,18 @@ function App({filmsCount, title, genre, year}: AppScreenProps): JSX.Element {
         </Route>
         <Route exact path='/login'>
           <UserPage />
+        </Route>
+        <Route exact path='/mylist'>
+          <MyListPage />
+        </Route>
+        <Route exact path='/films/:id'>
+          <MoviePage />
+        </Route>
+        <Route exact path='/films/:id/review'>
+          <AddReviewPage />
+        </Route>
+        <Route exact path='/player/:id'>
+          <PlayerPage />
         </Route>
       </Switch>
     </BrowserRouter>
