cef3c55c90e773f9344ef41638a6993a4d19ebf2
Tanja <tanja.slo@ukr.net>
2021-10-23 20:15:15 +0200

    Adds user-block component.

--- project/src/components/pages/add-review-page/add-review-page.tsx
+++ project/src/components/pages/add-review-page/add-review-page.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { Film } from '../../../types/film';
 import Breadcrumbs from '../../breadcrumbs/breadcrumbs';
 import Logo from '../../logo/logo';
+import UserBlock from '../../user-block/user-block';
 import NotFoundPage from '../not-found-page/not-found-page';
 
 type FilmsListProps = {
@@ -30,17 +31,7 @@ function AddReviewPage({films}: FilmsListProps): JSX.Element {
           <header className="page-header">
             <Logo />
             <Breadcrumbs id={id} name={name} />
-
-            <ul className="user-block">
-              <li className="user-block__item">
-                <div className="user-block__avatar">
-                  <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-                </div>
-              </li>
-              <li className="user-block__item">
-                <a href="/" className="user-block__link">Sign out</a>
-              </li>
-            </ul>
+            <UserBlock />
           </header>
 
           <div className="film-card__poster film-card__poster--small">
