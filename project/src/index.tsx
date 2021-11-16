534809b8a7de61449d38413c96e89ddc5f234087
Tanja <tanja.slo@ukr.net>
2021-11-16 14:20:51 +0100

    Creates store.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { reducer } from './store/reducer';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { films } from '../src/mocks/films';
 import { reviews } from '../src/mocks/reviews';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      films = {films}
-      reviews = {reviews}
-    />
+    <Provider store={store}>
+      <App
+        films = {films}
+        reviews = {reviews}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
