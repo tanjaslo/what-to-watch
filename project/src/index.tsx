efcae5c41f47b8066c7cd31e57edb36997ac4038
Tanja <tanja.slo@ukr.net>
2021-11-16 14:42:27 +0100

    Adds support for the Redux DevTools.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer } from './store/reducer';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { films } from '../src/mocks/films';
 import { reviews } from '../src/mocks/reviews';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <React.StrictMode>
